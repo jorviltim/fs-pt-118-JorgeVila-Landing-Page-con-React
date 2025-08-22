49ce151ec81af80a14cd42daf82f2896171ba5e1
Jorge Vila <45121848+jorviltim@users.noreply.github.com>
2025-08-22 19:16:53 +0000

    feat: añadido futer queda ordenar pagina

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,13 @@
 const Footer = () => {
 	return (
 		<div >
-            holita Footer
+			<nav class="navbar  navbar-expand-sm navbar-dark bg-dark">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">Footer</a>
+					
+					
+				</div>
+			</nav>
 		</div>
 	);
 };
